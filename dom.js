d2a3f1eef7ba9184600ea70419cacd70b871f861
Yashika <yashika7991@gmail.com>
2022-07-01 18:48:02 +0530

    dom

---
+++ dom.js
@@ -0,0 +1,6 @@
+var headerTitle =document.getElementById('header-title');
+var header =document.getElementById('main-header');
+header.style.borderBottom ='solid 3px #000';
+var headers=document.querySelector('.title');
+headers.style.fontWeight='bold';
+headers.style.color='green';
