a32e8210bdac4d3320cd0dd75b146a58d662c377
Yashika <yashika7991@gmail.com>
2022-07-01 22:25:40 +0530

    index2

--- demo2.js
+++ demo2.js
@@ -1,1 +1,18 @@
-console.log('I am Yashika');
+//Query selector
+
+
+var item= document.querySelector('.list-group-item:nth-child(2)');
+item.style.backgroundColor ='green';
+
+var item3= document.querySelector('.list-group-item:nth-child(3)');
+item3.style.display='none';
+
+//Query Selectorall
+
+var items= document.querySelectorAll('.list-group-item');
+items[1].style.color ='green';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+for(var i =0;i<odd.length;i++){
+    odd[i].style.backgroundColor = 'green';
+}
