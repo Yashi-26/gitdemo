07ad5586ad6e4c856eacbab667238dc555f55ed1
Yashika <yashika7991@gmail.com>
2022-07-01 19:14:11 +0530

    dom 2

---
+++ dom2.js
@@ -0,0 +1,7 @@
+
+var itms =document.getElementsByClassName('list-group-item');
+itms[2].style.fontWeight="bold";
+itms[2].style.backgroundColor="green";
+for(var i =0;i< itms.length;i++){
+    itms[i].style.fontWeight="bold";
+}
