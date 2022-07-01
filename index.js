3733869eb51553ff1dafc5f672d910eb1570cbe6
Yashika <yashika7991@gmail.com>
2022-07-01 19:31:20 +0530

    index

---
+++ index.js
@@ -0,0 +1,9 @@
+
+var li =document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[2].style.fontWeight="bold";
+li[2].style.backgroundColor="yellow";
+for(var i =0;i< li.length;i++){
+    li[i].style.fontWeight="bold";
+}
