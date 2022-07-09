05411c7704af2f09c9e29517fc42b915215b134d
Yashika <yashika7991@gmail.com>
2022-07-09 21:28:23 +0530

    promise

--- promise.js
+++ promise.js
@@ -2,8 +2,7 @@ const posts=[
     {titles: 'Post One', body:'This is post one'},
 
     {titles: 'Post Two', body:'This is post two'}
-    ];
-    
+    ];    
     function getPosts()
     { 
         setTimeout(() => 
