6198c8379154edef119cbca3c182320055adda67
Yashika <yashika7991@gmail.com>
2022-07-09 20:57:50 +0530

    promise

---
+++ promise1.js
@@ -0,0 +1,35 @@
+
+createPost()
+{
+    return new promise(() =>{
+        //update lastactivitytime
+    })
+}
+seensomeoneMessage()
+{
+    return new promise(() =>{
+        //update lastactivitytime
+    })
+}
+const user ={
+    username : 'shreya',
+    lastactivitytime:'13 th jan'
+}
+updatelastactivitytime =
+{
+    return new Promise((resolve,reject) ={
+        setTimeOut(()=>{
+            user.lastactivitytime = new Date().getTime();
+            resolve()
+        },1000)
+    })
+}
+
+userupdatesapost()
+{
+    Promise.all([createPost,updatelastactivitytime])
+    .then(([createPostresolves,updatelastactivitytimeresolves]) => {
+        console.log(updatelastactivitytimeresolves)
+    })
+    .catch(err => console.log(err))
+}
